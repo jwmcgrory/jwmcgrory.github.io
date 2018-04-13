6c65994d118b262033bbe69139c62febbc57fe08
jwmcgrory <jwmcgrory@gmail.com>
2018-04-13 11:36:28 -0600

    added links and icons

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-  $('.carousel').slick({
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    arrows: false,
-    draggable: false,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
 
  var lastScrollTop = 0;
  $(window).scroll(function(){
